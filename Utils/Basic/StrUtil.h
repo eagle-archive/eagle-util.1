5ebc4c97e41861805270009eb0e447caa96525fc
dynuaa <dynuaa@gmail.com>
2013-02-21 21:00:35 +0800

    Utils/Basic/StrUtil: s2ws() + ws2s()

--- Utils/Basic/StrUtil.h
+++ Utils/Basic/StrUtil.h
@@ -8,6 +8,9 @@ namespace utils {
 bool WideStrToAnsiStr(std::string &str, const WCHAR * wstr);
 void StringReplace(std::string &strBase, const std::string &strSrc, const std::string &strDes);
 
+std::wstring s2ws(const std::string& s);
+std::string ws2s(const std::wstring& s);
+
 std::string &TrimStr(const std::string& str, const char *whitespace = " \t");
 std::string &ReduceStr(const std::string& str, const char *fill = " ", const char *whitespace = " \t");
 
--- Utils/Basic/StrUtilWin.cpp
+++ Utils/Basic/StrUtilWin.cpp
@@ -39,6 +39,26 @@ bool WideStrToAnsiStr(std::string &str, const WCHAR * wstr)
     return result;
 }
 
+std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
+    std::wstring r(len, L'\0');
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, &r[0], len);
+    return r;
+}
+
+std::string ws2s(const std::wstring& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0); 
+    std::string r(len, '\0');
+    WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, &r[0], len, 0, 0); 
+    return r;
+}
+
 void StringReplace(string &strBase, const string &strSrc, const string &strDes)
 {
     string::size_type pos = 0;
