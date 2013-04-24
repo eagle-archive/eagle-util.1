59d9bc110a957f9229aa6f8090de143a5d78de22
dynuaa <dynuaa@gmail.com>
2013-04-24 23:21:20 +0800

    Java/TestTaxiStub/src/com/sap/nic/mytaxi/cityaccess: update for javadoc

--- Java/TestTaxiStub/src/com/sap/nic/mytaxi/cityaccess/TaxiOrderException.java
+++ Java/TestTaxiStub/src/com/sap/nic/mytaxi/cityaccess/TaxiOrderException.java
@@ -9,14 +9,14 @@ public class TaxiOrderException extends RuntimeException {
 	 * description should contain the device ID.<br>
 	 * 错误代码：设备标志没有找到。description参数中应包含出错的设备标志。
 	 */
-	public static final int ERR_NO_DEVICE_NOT_FOUND = 100;
+	public static final int ERRNO_DEVICE_NOT_FOUND = 100;
 
 	/**
 	 * Error No: order not found<br>
 	 * description should contain the order ID.<br>
 	 * 错误代码：订单号没有找到。description参数中应包含该订单号。
 	 */
-	public static final int ERR_NO_ORDER_NOT_FOUND = 102;
+	public static final int ERRNO_ORDER_NOT_FOUND = 101;
 
 	/**
 	 * Error No: invalid order operation<br>
@@ -24,9 +24,28 @@ public class TaxiOrderException extends RuntimeException {
 	 * 错误代码：操作不合理，例如用户取消招车，但是当前订单不是司机确认状态。<br>
 	 * description参数中应包含订单状态，用户操作，简单描述为什么操作不合理。
 	 */
-	public static final int ERR_NO_INVALID_ORDER_OP = 102;
+	public static final int ERRNO_INVALID_ORDER_OP = 102;
 
-	private static final long serialVersionUID = -4614109358656973236L;
+	/**
+	 * Error No: database operation error.<br>
+	 * description should contain details.<br>
+	 * 错误代码：数据库错误。<br>
+	 * description参数中应包含具体的错误信息。
+	 */
+	public static final int ERRNO_DB_OPERATION_ERROR = 103;
+
+	/**
+	 * Error No: network error.<br>
+	 * description should contain details.<br>
+	 * 错误代码：网络错误。<br>
+	 * description参数中应包含具体的错误信息。
+	 */
+	public static final int ERRNO_NETWORK_ERROR = 104;
+
+
+	// add new error numbers above
+
+	private static final long serialVersionUID = -4107483338734046173L;
 	protected long errorCode;
 
 	/**
--- Java/TestTaxiStub/src/com/sap/nic/mytaxi/cityaccess/TaxiTrackException.java
+++ Java/TestTaxiStub/src/com/sap/nic/mytaxi/cityaccess/TaxiTrackException.java
@@ -1,7 +1,7 @@
 package com.sap.nic.mytaxi.cityaccess;
 
 /**
- * This exception is thrown when taxi tracking error happens
+ * This exception is thrown when taxi tracking error happens.
  */
 public class TaxiTrackException extends RuntimeException {
 	/**
@@ -9,15 +9,33 @@ public class TaxiTrackException extends RuntimeException {
 	 * description should contain the device ID.<br>
 	 * 错误代码：设备标志没有找到。description参数中应包含出错的设备标志。
 	 */
-	public static final int ERR_NO_DEVICE_NOT_FOUND = 200;
+	public static final int ERRNO_DEVICE_NOT_FOUND = 200;
 
 	/**
 	 * Error No: function not supported<br>
 	 * description should contain what function is not supported.<br>
 	 * 错误代码：功能不支持。description参数中应说明具体什么功能不支持。
 	 */
-	public static final int ERR_NO_FUNCTION_NOT_SUPPORTED = 201;
+	public static final int ERRNO_FUNCTION_NOT_SUPPORTED = 201;
 
+	/**
+	 * Error No: database operation error.<br>
+	 * description should contain details.<br>
+	 * 错误代码：数据库错误。<br>
+	 * description参数中应包含具体的错误信息。
+	 */
+	public static final int ERRNO_DB_OPERATION_ERROR = 202;
+
+	/**
+	 * Error No: network error.<br>
+	 * description should contain details.<br>
+	 * 错误代码：网络错误。<br>
+	 * description参数中应包含具体的错误信息。
+	 */
+	public static final int ERRNO_NETWORK_ERROR = 203;
+
+
+	// add new error numbers above
 
 	private static final long serialVersionUID = -8855254829945924195L;
 	protected long errorCode;
