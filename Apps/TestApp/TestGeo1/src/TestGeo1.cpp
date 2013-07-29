ac3ff047c8e62acce1a9afe1191fb72f22d735c7
dynuaa <dynuaa@gmail.com>
2013-07-29 11:31:33 +0800

    Apps/TestApp/TestGeo1: initial add

---
+++ Apps/TestApp/TestGeo1/src/TestGeo1.cpp
@@ -0,0 +1,18 @@
+// TestHanaOdbc.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "insert_sim_utils.h"
+
+bool bulk_insert(const char *csv);
+
+int main()
+{
+    if (false == init_globals(CONFIG_FILE)) {
+        printf("Error: cannot load config file %s\n", CONFIG_FILE);
+        return 10;
+    }
+
+
+	return 0;
+}
